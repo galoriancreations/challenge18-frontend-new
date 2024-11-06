4177a0e91c9201fe30e201cfabaf4607f0b5be28
uriHenig <uryhenig@gmail.com>
2024-11-06 22:48:56 +0200

    created app bar and sideBar

--- src/app/appBar.tsx
+++ src/app/appBar.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { BiSearch } from "react-icons/bi";
 
-const appBar = () => {
+const AppBar = () => {
   return (
-    <header className="bg-primary text-textPrimary p-4 shadow-lg flex justify-between items-center">
-      <Link href="/">
-        <div className="h-full aspect-auto">
-          <Image src={"/logo.png"} alt="Company Logo" className="h-full object-contain" width={50} height={50} />
-        </div>
+    <header className="bg-primary text-textPrimary p-4 shadow-lg flex justify-between items-center h-16">
+      <Link href="/" className="flex items-center h-full">
+        <Image src="/logo.png" alt="Company Logo" className="object-contain" width={70} height={70} />
       </Link>
-      <nav>
-        <ul className="flex px-3 gap-4">
-          <li>
-            <input type="text" placeholder="Search" />
-          </li>
-          <li>
-            <a href="#" className="hover:text-secondary">
-              LOG IN
-            </a>
-          </li>
-        </ul>
-      </nav>{" "}
-      <button className="bg-secondary text-primary font-semibold px-4 py-2 rounded">Sign Up</button>
+
+      <div className="relative flex-1 flex justify-center max-w-md">
+        <span className="absolute inset-y-0 left-4 flex items-center text-gray-500">
+          <BiSearch size={20} />
+        </span>
+        <input
+          className="py-2 pl-10 pr-4 w-full rounded-full bg-white text-primary shadow-md placeholder-primary placeholder-opacity-70"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+
+      <nav className="flex items-center gap-4">
+        <a href="#" className="hover:text-secondary font-semibold">
+          LOG IN
+        </a>
+        <button className="bg-secondary text-primary font-semibold px-4 py-2 rounded">Sign Up</button>
+      </nav>
     </header>
   );
 };
 
-export default appBar;
+export default AppBar;
--- src/app/sideBar.tsx
+++ src/app/sideBar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { TbSchool } from "react-icons/tb";
 import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 import { BiSolidSchool } from "react-icons/bi";
@@ -6,65 +8,115 @@ import { HiOutlineLightBulb } from "react-icons/hi";
 import { GoGraph } from "react-icons/go";
 import { CiGlobe } from "react-icons/ci";
 
-const Sidebar = () => (
-  <aside className="w-64 h-screen bg-primary text-textPrimary  fixed">
-    <ul className="space-y-4">
-      <li>
-        <a href="#" className="flex items-center w-full gap-4 mt-6 px-6 py-2  hover:bg-accent hover:text-white">
-          <span>
-            <LiaChalkboardTeacherSolid size={25} />
-          </span>
-          <span>Educators</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" className="flex items-center gap-4 px-6 py-2   hover:bg-accent hover:text-white">
-          <span>
-            <TbSchool size={25} />
-          </span>
-          <span>Students</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" className="flex items-center gap-4 px-6 py-2   hover:bg-accent hover:text-white">
-          <span>
-            <BiSolidSchool size={25} />
-          </span>
-          <span>Institutions</span>{" "}
-        </a>
-      </li>
-      <li>
-        <a href="#" className="flex items-center gap-4 px-6 py-2  hover:bg-accent hover:text-textPrimary">
-          <span>
-            <HiOutlineLightBulb size={25} />
-          </span>
-          <span>Ed-Tech Startups</span>{" "}
-        </a>
-      </li>
-      <li>
-        <a href="#" className="flex items-center gap-4 px-6 py-2   hover:bg-accent hover:text-white">
-          <span>
-            <GoGraph size={25} />
-          </span>
-          <span>Investors</span>{" "}
-        </a>
-      </li>
-      <li>
-        <a href="#" className="flex items-center gap-4 px-6 py-2   hover:bg-accent hover:text-white">
-          <span>
-            <CiGlobe size={25} />
-          </span>
-          <span>Government and NGO</span>{" "}
-        </a>
-      </li>
-      <li>
-        <a href="#" className="flex items-center gap-4 px-6 py-2   hover:bg-accent hover:text-white">
-          <span>ℹ️</span>
-          <span>About</span>
-        </a>
-      </li>
-    </ul>
-  </aside>
-);
+const Sidebar = () => {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <aside className="w-52 h-screen bg-primary text-textPrimary fixed">
+      <ul className="space-y-4">
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("educators")}
+            className={`flex items-center w-full gap-4 mt-6 px-6 py-2 ${
+              activeLink === "educators" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>
+              <LiaChalkboardTeacherSolid size={25} />
+            </span>
+            <span>Educators</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("students")}
+            className={`flex items-center gap-4 px-6 py-2 ${
+              activeLink === "students" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>
+              <TbSchool size={25} />
+            </span>
+            <span>Students</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("institutions")}
+            className={`flex items-center gap-4 px-6 py-2 ${
+              activeLink === "institutions" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>
+              <BiSolidSchool size={25} />
+            </span>
+            <span>Institutions</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("edtech")}
+            className={`flex items-center gap-4 px-6 py-2 ${
+              activeLink === "edtech" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>
+              <HiOutlineLightBulb size={25} />
+            </span>
+            <span>Ed-Tech Startups</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("investors")}
+            className={`flex items-center gap-4 px-6 py-2 ${
+              activeLink === "investors" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>
+              <GoGraph size={25} />
+            </span>
+            <span>Investors</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("government")}
+            className={`flex items-center gap-4 px-6 py-2 ${
+              activeLink === "government" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>
+              <CiGlobe size={25} />
+            </span>
+            <span>Government and NGO</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() => handleLinkClick("about")}
+            className={`flex items-center gap-4 px-6 py-2 ${
+              activeLink === "about" ? "hover:bg-accent text-secondary" : "hover:bg-accent hover:text-white"
+            }`}
+          >
+            <span>ℹ️</span>
+            <span>About</span>
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+};
 
 export default Sidebar;
