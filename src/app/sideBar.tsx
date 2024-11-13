620f7313884bdde0b250a8eeb4e3f9aaf66c316e
Uri <uryhenig@gmail.com>
2024-11-13 22:12:50 +0200

    removed comments

--- src/app/sideBar.tsx
+++ src/app/sideBar.tsx
@@ -45,8 +45,4 @@ const Sidebar = () => {
   );
 };
 
-// `flex items-center w-full gap-4 mt-6 px-6 py-2 hover:bg-accent ${
-//               link.href === currentPath ? " text-secondary" : " hover:text-white"
-//             }`
-
 export default Sidebar;
